d0ff1e7cd2fc4d4ade7d31d5eb8ffc843886ac1f
Annika <85462906+AnniK93@users.noreply.github.com>
2022-04-09 00:07:44 +0100

    Added API

---
+++ script.js
@@ -0,0 +1,16 @@
+function showAdvice(response) {
+  let h2Element = document.querySelector("h2");
+  h2Element.innerHTML = `${response.data.slip.advice}`;
+
+  let h3Element = document.querySelector("h3");
+  h3Element.innerHTML = `Advice #${response.data.slip.id}`;
+}
+
+function searchAdvice(event) {
+  event.preventDefault();
+  let apiUrl = "https://api.adviceslip.com/advice";
+  axios.get(apiUrl).then(showAdvice);
+}
+
+let selectDice = document.querySelector("#dice");
+selectDice.addEventListener("click", searchAdvice);

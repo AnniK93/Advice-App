679efb2cedf29e30a428ff9508dbb54db155fb63
Annika <85462906+AnniK93@users.noreply.github.com>
2022-04-10 17:28:10 +0100

    Improved API

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 function showAdvice(response) {
+  console.log(response);
   let h2Element = document.querySelector("h2");
   h2Element.innerHTML = `"${response.data.slip.advice}"`;
 
-  let h3Element = document.querySelector("h3");
-  h3Element.innerHTML = `Advice #${response.data.slip.id}`;
+  let h1Element = document.querySelector("h1");
+  h1Element.innerHTML = `Advice #${response.data.slip.id}`;
 }
 
 function searchAdvice(event) {
   event.preventDefault();
-  let apiUrl = "https://api.adviceslip.com/advice";
+  let id = Math.floor(Math.random() * 255);
+  let apiUrl = `https://api.adviceslip.com/advice/${id}`;
   axios.get(apiUrl).then(showAdvice);
 }
 
